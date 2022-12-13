754d34f0b11ebcf932c6d2bb2b64e5ee8f1fec9d
Murerwa2063 <kemurerwa@gmail.com>
2022-12-13 11:13:30 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ * Return:
+ * 0 (success)
+ * non-zero if an error occurred
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+	printf("%d", i);
+putchar(i);
+
+return (0);
+}
