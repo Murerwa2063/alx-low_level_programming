7b6ba2c49b4c8cc7eee6c8377eb9dbbee1089d0c
Murerwa2063 <kemurerwa@gmail.com>
2022-12-13 12:04:00 -0800

    print numbers with base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ * Return:
+ * 0 (success)
+ * non-zefo of an errror occurred
+ */
+
+int main(void)
+{
+	unsigned char c = '0';
+	int a = 0;
+
+	for (; a < 10; a++)
+{
+	putchar(c);
+	c++;
+}
+c = '1';
+for (; a < 6; a++)
+{
+	putchar('0' + c);
+	c++;
+}
+putchar('\n');
+return (0);
+}
