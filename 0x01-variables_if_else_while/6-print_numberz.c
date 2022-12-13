e70eda46a653e2573f3c579f47c0659c1bc09146
Murerwa2063 <kemurerwa@gmail.com>
2022-12-13 11:23:19 -0800

    print all single digit numbers of baee 10 starting from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ * Return:
+ * 0 (success)
+ */
+
+int main(void)
+{
+int i = 0;
+for (; i < 10; i++)
+	putchar('0' + i);
+putchar('\n');
+return (0);
+}
