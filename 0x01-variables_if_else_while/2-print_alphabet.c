20aaf2c2cb5054a3b581cf3b92ea1c3978455976
Murerwa2063 <kemurerwa@gmail.com>
2022-12-13 09:11:39 -0800

    print lowercase alphabet using putchar function only

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Starting point
+ *
+ * Return:
+ * 0 (success)
+ * non-zero if an error occurred
+ */
+main(void)
+
+{
+int ch;
+	for (ch = "a"; ch <= "z"; ch++)
+		putchar(ch);
+	putchar("\n")
+		return (0);
+}
