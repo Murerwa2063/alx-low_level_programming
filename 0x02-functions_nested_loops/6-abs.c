dd0217a9d6d7890db3d41b3042a0d291e72a63b1
root <root@DESKTOP-I821JLU.localdomain>
2022-12-14 23:07:27 +0300

    a function that computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _abs - Computes the absolute value of an integer
+  * @n: integer being computed
+  * Return:
+  * int
+  * int * negative
+  */
+
+int _abs(int n)
+
+{
+
+	if (n > 0)
+		return (n);
+	else
+		return (n * -1);
+}
