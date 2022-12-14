edf35d7d6ca71836ec2082a5311781143579aece
root <root@DESKTOP-I821JLU.localdomain>
2022-12-14 16:29:18 +0300

    added main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <main.h>
 /**
  * main - Starting point
  * Return:
@@ -7,9 +8,9 @@
  */
 int main(void)
 {
-	char a = _putchar;
+	char a;
 
-	putchar(a);
-	putchar('\n');
+	_putchar(a);
+	_putchar('\n');
 	return (0);
 }
