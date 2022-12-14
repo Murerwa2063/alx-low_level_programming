1c88808f2dfc2fc47b169eadd5e289aa70798727
root <root@DESKTOP-I821JLU.localdomain>
2022-12-14 16:25:49 +0300

    Learning functions and nested loops in C

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ * Return:
+ * 0 (success)
+ * 1 if an error occurred
+ */
+int main(void)
+{
+	char a = _putchar;
+
+	putchar(a);
+	putchar('\n');
+	return (0);
+}
