898ece6b490bce5caaba25ae64df90823b044883
root <root@DESKTOP-I821JLU.localdomain>
2022-12-15 01:08:31 +0300

    a function that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adding up function.
+ * @i: First integer
+ * @j: Second integer
+ * Return: sum of both integers
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
