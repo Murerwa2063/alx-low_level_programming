0d56351fab0575d96f2e813fb9cc8c4c912d7351
root <root@DESKTOP-I821JLU.localdomain>
2022-12-16 01:49:23 +0300

    9-fizz_buzz.c fixed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 /**
- * main - Entry point. prints the numbers from 1 to 100, followed by a new line
+ * main - main function
  *
  * Return: 0 (success); 1 otherwise
  */
+#include <stdio.h>
+
 int main(void)
 {
 	int i;
 
-	for (i = 1; i < 101; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz ");
-		}
-		else if (i % 3 == 0)
+		} else if (i % 3 == 0)
 		{
 			printf("Fizz ");
-		}
-		else if (i % 5 == 0)
+		} else if (i % 5 == 0)
 		{
 			printf("Buzz ");
-		}
-		else
+		} else
 		{
 			printf("%d ", i);
 		}
