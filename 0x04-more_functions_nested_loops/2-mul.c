0a341c78855aa7e0d0f1b013836d758571462688
root <root@DESKTOP-I821JLU.localdomain>
2022-12-15 19:41:14 +0300

    a function to multiply 2 integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *  mul - function. multiplies two integers
+ *  @a: First integer
+ *  @b: Second integer
+ *
+ *  Return:
+ *  0 (success)
+ *  1 otherwise
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
