356f2661c743f98692e474259d06811bb1cfd1fc
Murerwa2063 <kemurerwa@gmail.com>
2022-12-20 03:10:57 -0800

    funtion prints length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+/**
+ * _strlen - checks string length
+ * @s: string being checked
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
+
