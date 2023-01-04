4b08b87a4c4518f4ca161d706cab736b406c823c
Murerwa2063 <kemurerwa@gmail.com>
2023-01-04 21:59:06 +0300

    switched to while loop

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,18 +12,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+	while (*s)
+	{
+		if (*s != c)
+		s++;
+		else
+		return (s);
+	}
+	if (c == '\0')
+	return (s);
 
-	for (i = 0; s[i] != '\0'; i++)
-		{
-		if (s[i] == c)
-		{
-			return (&s[i]);
-		}
-		}
-		if (c == '\0')
-		{
-			return (&s[i]);
-		}
 	return (NULL);
 }
