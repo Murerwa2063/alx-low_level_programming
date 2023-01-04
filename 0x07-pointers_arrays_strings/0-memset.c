79ebf8be2b189976ec75a3e2513691feaab08de5
Murerwa2063 <kemurerwa@gmail.com>
2023-01-04 20:27:22 +0300

    fixed return value issue in documentation of 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,12 +2,13 @@
 /**
  * _memset - fills the first n bytes of the memory area pointed to by s with
  * the constant byte b
+ *
  * to by s with the constant byte b
  * @n: number of bytes to fill
  * @s: pointer to memory area to be filled
  * @b: constant byte to fill memory area with
  *
- * Reurn: a pointer to the memory area s
+ * Return: a pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
