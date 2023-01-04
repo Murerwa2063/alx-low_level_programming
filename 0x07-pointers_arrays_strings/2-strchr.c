8500b168975173e26e0fc3b4317b0fb3e409bb71
Murerwa2063 <kemurerwa@gmail.com>
2023-01-04 21:52:57 +0300

    fixed some errors in 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -15,11 +15,15 @@ char *_strchr(char *s, char c)
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
-	{
+		{
 		if (s[i] == c)
 		{
 			return (&s[i]);
 		}
-	}
+		}
+		if (c == '\0')
+		{
+			return (&s[i]);
+		}
 	return (NULL);
 }
