31e293caad59d31fc3b9293d079df10f19352c7b
Murerwa2063 <kemurerwa@gmail.com>
2023-03-03 17:33:55 +0300

    pointers, arrays and strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,18 +3,15 @@
  * print_rev - prints a string in reverse
  * @s: string to be printed
  */
+
 void print_rev(char *s)
 {
-int len = 0;
-while (s[len] != '\0')
-{
-	len++;
-}
+	int len = strlen(s);
+	int i;
 
-for (int i = len - 1; i >= 0; i--)
-{
-	_putchar("%c", s[i]);
-}
-
-_putchar("\n");
+	for (i = len - 1; i >= 0; i--)
+	{
+	_putchar("%s", s[i]);
+	}
+	_putchar("\n");
 }
