5ffc28a3dcf65cab1b7fa9e026200f8f8ad7ef53
Murerwa2063 <kemurerwa@gmail.com>
2023-03-26 14:55:31 +0300

    Write a function that prints numbers, followed by a new line.
    Prototype: void print_numbers(const char *separator, const unsigned int n, ...);
    where separator is the string to be printed between numbers
    and n is the number of integers passed to the function
    You are allowed to use printf
    If separator is NULL, don’t print it
    Print a new line at the end of your function

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers followed by a new line.
+ * @n: numbers
+ * @separator: string
+ * Return: numbers followed by a new line.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int x;
+	va_list args;
+	va_start(args, n);
+
+	for (x = 0; x < n; x++)
+	{
+		printf("%d", va_arg(args, int));
+
+	if (separator != NULL && x < n - 1)
+	{
+		printf("%s", separator);
+	}
+	}
+	va_end(args);
+	printf("\n");
+}
+
